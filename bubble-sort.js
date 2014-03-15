a75e2c80d483632ba9f03af4a64a84f6cd53b43f
zmike86 <zmike86@163.com>
2014-03-15 08:27:14 -0700

    use 2 for loops

--- bubble-sort.js
+++ bubble-sort.js
@@ -3,49 +3,59 @@
  * @author AceMood 
  * @email zmike86@gmail.com
  *
- * @License Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * @License 
+ * The MIT License (MIT)
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (c) 2014 AceMood
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
 /**
  * Order an array.
- * @param {Array} arr
- * @param {boolean} orderUp
- * @return {Array}
+ * @param {Array} arr An array to be sorted.
+ * @param {boolean} orderUp If ordering up or not.
+ * @return {Array} the result array.
  */ 
 function bubbleSort (arr, orderUp) {
-  var i = arr.length, j;
+  var i, j;
   var temp;
   var cmpFn = orderUp ? function (a, b) {
-	return a > b
+    return a > b
   } : function (a, b) {
-	return b > a
+    return b > a
   };
-  while (i > 0) {
-    for (j = 0; j < i - 1; j++) {
+  for (i = arr.length - 1 ; i >= 0; i--) {
+    for (j = 0; j < i; j++) {
       if (cmpFn(arr[j], arr[j + 1])) {
         temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
-    i--;
   }
   return arr
 }
 
 // Usage: 
-var arr = [3,2,4,9,1,5,7,6,8];
+var arr = [3, 2, 4, 9, 1, 5, 7, 6, 8];
 var arrSorted = bubbleSort(arr);
 
 console.log(arrSorted);
