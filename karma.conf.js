261c424e9e34df18787ff79f07fb6c09821c537f
unknown <zhangshen@ZHANGSHEN.sogou-inc.com>
2014-12-04 16:52:03 +0800

    cls test case

--- karma.conf.js
+++ karma.conf.js
@@ -2,70 +2,103 @@
 // Generated on Wed Dec 03 2014 21:59:03 GMT+0800 (中国标准时间)
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    var grid = {
+        hostname: '127.0.0.1',
+        port: 4444
+    };
 
+    config.set({
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      '**/*.js'
-    ],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files / patterns to load in the browser
+        files: [
+            '**/CommonLongestString.js', '**/CommonLongestString-spec.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // web server port
-    port: 9876,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // web server port
+        port: 9876,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // number ms delay to wait for other changes
-    autoWatchBatchDelay: 5000,
-
-    // time for connecting
-    browserDisconnectTolerance: 5000,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS', 'IE', 'Opera'],
-    browsers: ['Chrome'],
+        // cli runner port
+        // runnerPort: 9100,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // number ms delay to wait for other changes
+        autoWatchBatchDelay: 5000,
+
+
+        // time for connecting
+        browserDisconnectTolerance: 5000,
+
+
+        //
+        customLaunchers: {
+            'IE9-Win7': {
+                base: 'WebDriver',
+                config: grid,
+                browserName: 'internet explorer',
+                platform: 'Windows 7',
+                version: '9'
+            }
+        },
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        //browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS', 'IE', 'Opera'],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['IE'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
 };
--- 动态规划/CommonLongestString.js
+++ 动态规划/CommonLongestString.js
@@ -30,82 +30,95 @@
  * SOFTWARE.
  */
 
-// 两个字符串
-var word1, word2;
-var lcsarr;
+
+;(function(global) {
+
+    'use strict';
+
+    // 两个字符串
+    var word1, word2;
+    var lcsarr;
 
 // 保存最大公共串长度
-var max = 0;
-var index = 0;
+    var max = 0;
+    var index = 0;
 
 // 打印结果
-function print(ret) {
-    var str = "";
-    for(var i = 0; i <= word2.length; ++i) {
-        for(var j = 0; j <= word1.length; ++j) {
-            str += ("   " + lcsarr[j][i]);
+    function print(ret) {
+        var str = "";
+        for(var i = 0; i <= word2.length; ++i) {
+            for(var j = 0; j <= word1.length; ++j) {
+                str += ("   " + lcsarr[j][i]);
+            }
+            str += "\n";
         }
-        str += "\n";
+
+        console.log(str);
+
+        console.log("\n The result is: " + ret);
     }
-
-    console.log(str);
-
-    console.log("\n The result is: " + ret);
-}
 
 // 初始化二维数组各项为0
-function init() {
-    lcsarr = new Array(word1.length + 1);
-    for(var i = 0; i <= word1.length; ++i) {
-        lcsarr[i] = new Array(word2.length + 1);
-        for(var j = 0; j <= word2.length; ++j) {
-            lcsarr[i][j] = 0;
+    function init() {
+        lcsarr = new Array(word1.length + 1);
+        for(var i = 0; i <= word1.length; ++i) {
+            lcsarr[i] = new Array(word2.length + 1);
+            for(var j = 0; j <= word2.length; ++j) {
+                lcsarr[i][j] = 0;
+            }
         }
     }
-}
 
 // 算法主体部分
-function exec() {
-    for (var i = 0; i <= word1.length; ++i) {
-        for(var j = 0; j <= word2.length; ++j) {
-            if (i == 0 || j == 0) {
-                lcsarr[i][j] = 0;
-            } else {
-                if (word1[i - 1] == word2[j - 1]) {
-                    lcsarr[i][j] = lcsarr[i - 1][j - 1] + 1;
-                } else {
+    function exec() {
+        for (var i = 0; i <= word1.length; ++i) {
+            for(var j = 0; j <= word2.length; ++j) {
+                if (i == 0 || j == 0) {
                     lcsarr[i][j] = 0;
+                } else {
+                    if (word1[i - 1] == word2[j - 1]) {
+                        lcsarr[i][j] = lcsarr[i - 1][j - 1] + 1;
+                    } else {
+                        lcsarr[i][j] = 0;
+                    }
+                }
+                if (max < lcsarr[i][j]) {
+                    max = lcsarr[i][j];
+                    index = i;
                 }
             }
-            if (max < lcsarr[i][j]) {
-                max = lcsarr[i][j];
-                index = i;
+        }
+        var str = "";
+        if (max == 0) {
+            return "";
+        } else {
+            for (var i = index - max; i <= max; ++i) {
+                str += word2[i];
             }
+            return str;
         }
-    }
-    var str = "";
-    if (max == 0) {
-        return "";
-    } else {
-        for (var i = index - max; i <= max; ++i) {
-            str += word2[i];
-        }
-        return str;
-    }
 
-}
+    }
 
 // 主入口函数, 提供两个长字符串
-function lcs(w1, w2) {
-    if (!w1 || !w2) return;
+    function lcs(w1, w2) {
+        if (!w1 || !w2) return;
 
-    word1 = w1;
-    word2 = w2;
-    // 初始化
-    init();
-    // 主体执行部分
-    var ret = exec();
-    print(ret);
-}
+        word1 = w1;
+        word2 = w2;
+        // 初始化
+        init();
+        // 主体执行部分
+        var ret = exec();
+        print(ret);
+
+        return ret;
+    }
+
+    global.lcs = lcs;
+
+})(this);
+
+
 
 // lcs("abbcc", "dbbcc");
